78974064c89ac9c87db0475af33003019c1ffa93
Utshant Gurung <utshantgurung@gmail.com>
2021-04-30 09:30:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  if (index.between?(0,8)) && (position_taken?(board, index) == false)
+  if (index.between?(0,8)) && (position_taken?(board, index) != true)
     true
   else
     false
